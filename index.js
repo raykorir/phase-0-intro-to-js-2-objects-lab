df8be5d8309eeefeca57075916bab95881278673
raymond <raymond.korir@student.moringaschool.com>
2024-04-01 02:27:52 +0300

    Object-lab

--- index.js
+++ index.js
@@ -1,1 +1,25 @@
 // Write your solution in this file!
+function updateEmployeeWithKeyAndValue(employee, key, value) {
+    const updatedEmployee = { ...employee };
+    updatedEmployee[key] = value;
+    return updatedEmployee;
+}
+function destructivelyUpdateEmployeeWithKeyAndValue(employee, key, value) {
+    employee[key] = value;
+    return employee;
+}
+function deleteFromEmployeeByKey(employee, key) {
+    const clonedEmployee = { ...employee };
+    delete clonedEmployee[key];
+    return clonedEmployee;
+}
+
+function destructivelyDeleteFromEmployeeByKey(employee, key) {
+    delete employee[key];
+    return employee;
+}
+
+const employee = {
+    name: 'John Doe',
+    streetAddress: '123 Main St'
+};
